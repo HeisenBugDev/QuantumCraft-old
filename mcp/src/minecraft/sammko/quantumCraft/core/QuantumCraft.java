e05e3636fa1a08e7eb4c771d29ec1d3bf375180c
Theron Boerner <Theron@theron-desktop.(none)>
2013-01-23 11:04:38 -0600

    Test commit

--- mcp/src/minecraft/sammko/quantumCraft/core/QuantumCraft.java
+++ mcp/src/minecraft/sammko/quantumCraft/core/QuantumCraft.java
@@ -36,7 +36,7 @@ import sammko.quantumCraft.CommonProxy;
 @NetworkMod(clientSideRequired=true, serverSideRequired=false)
 public class QuantumCraft {
 
-
+//MainClass 
 	
 	public WorldGenOres worldGen = new WorldGenOres();
 	
