a85261ecdc32b9a3beb9c9dc6d1f596744a0569a
Theron Boerner <hunterboerner@gmail.com>
2013-01-28 10:21:39 -0600

    update clienttickhandler

--- mcp/src/minecraft/sammko/quantumCraft/client/ClientProxy.java
+++ mcp/src/minecraft/sammko/quantumCraft/client/ClientProxy.java
@@ -20,6 +20,7 @@ public class ClientProxy extends CommonProxy {
 		 
 	}
 	
+	
 
 	
 }
--- mcp/src/minecraft/sammko/quantumCraft/client/ClientTickHandler.java
+++ mcp/src/minecraft/sammko/quantumCraft/client/ClientTickHandler.java
@@ -1,52 +1,46 @@
 package sammko.quantumCraft.client;
 
-import cpw.mods.fml.common.ITickHandler;
-import cpw.mods.fml.common.TickType;
 import java.util.EnumSet;
 
-import sammko.quantumCraft.core.QuantumCraftSettings;
-import sammko.quantumCraft.items.ItemInitializator;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.entity.EntityClientPlayerMP;
-import net.minecraft.client.gui.FontRenderer;
-import net.minecraft.client.gui.GuiIngame;
-import net.minecraft.client.gui.ScaledResolution;
-import net.minecraft.client.settings.GameSettings;
-import net.minecraft.client.settings.KeyBinding;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.InventoryPlayer;
-import net.minecraft.entity.player.PlayerCapabilities;
 import net.minecraft.item.ItemStack;
 import net.minecraft.src.ModLoader;
+import sammko.quantumCraft.core.QuantumCraftSettings;
+import cpw.mods.fml.common.ITickHandler;
+import cpw.mods.fml.common.TickType;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class ClientTickHandler implements ITickHandler {
-
+	@SideOnly(Side.CLIENT)
 	public static Minecraft mc = ModLoader.getMinecraftInstance();
+	
 
 	public EnumSet ticks() {
 		
-		return EnumSet.of(TickType.WORLD, TickType.WORLDLOAD, TickType.CLIENT,
-				TickType.RENDER);
+		return EnumSet.of(TickType.CLIENT);
 	}
 	
 	public String getItemDamage(ItemStack item) {
+	
 		if (mc.inGameHasFocus == true && mc.thePlayer.inventory.getCurrentItem() != null && mc.thePlayer.inventory.getCurrentItem().itemID == QuantumCraftSettings.CrystalPickaxeID + 256 ) {
 			final int damage = item.getItemDamageForDisplay();
 			final int damageLeft = 500-damage;
 			String damageString = null;
 			if (damageLeft >= 300){
-				damageString = "§a" + damageLeft + "/500";
+				damageString = "Â§a" + damageLeft + "/500";
 			}
 			else if (damageLeft >= 100){
-				damageString = "§e" + damageLeft + "/500";
+				damageString = "Â§e" + damageLeft + "/500";
 			}
 			else if (damageLeft >= 50){
-				damageString = "§5" + damageLeft + "/500";
+				damageString = "Â§5" + damageLeft + "/500";
 			}
 			return damageString;
 		}else{
 			return "";
 		}
+		
 	}
 
 	@Override
@@ -57,12 +51,13 @@ public class ClientTickHandler implements ITickHandler {
 
 	@Override
 	public void tickEnd(EnumSet<TickType> type, Object... tickData) {
+		if((type.contains(TickType.CLIENT))&& (mc.theWorld != null)) {
 		if (mc.inGameHasFocus == true) {
 			ItemStack itemstack = mc.thePlayer.inventory.getCurrentItem();
 			mc.fontRenderer.drawString(getItemDamage(itemstack), 1, 1, 1);
 			//mc.fontRenderer.drawString("test", 1, 1, 1);
 		}
-
+		}
 	}
 
 	@Override
