9a88854342423fac0017a1eedbdd65c673298d88
Theron Boerner <hunterboerner@gmail.com>
2013-01-28 10:40:18 -0600

    Update ClientHandler

--- mcp/src/minecraft/sammko/quantumCraft/client/ClientTickHandler.java
+++ mcp/src/minecraft/sammko/quantumCraft/client/ClientTickHandler.java
@@ -6,58 +6,59 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.item.ItemStack;
 import net.minecraft.src.ModLoader;
 import sammko.quantumCraft.core.QuantumCraftSettings;
+import cpw.mods.fml.client.FMLClientHandler;
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.ITickHandler;
 import cpw.mods.fml.common.TickType;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 public class ClientTickHandler implements ITickHandler {
+
 	@SideOnly(Side.CLIENT)
-	public static Minecraft mc = ModLoader.getMinecraftInstance();
-	
+	Minecraft mc = FMLClientHandler.instance().getClient();
 
 	public EnumSet ticks() {
-		
+
 		return EnumSet.of(TickType.CLIENT);
 	}
-	
+
 	public String getItemDamage(ItemStack item) {
-	
-		if (mc.inGameHasFocus == true && mc.thePlayer.inventory.getCurrentItem() != null && mc.thePlayer.inventory.getCurrentItem().itemID == QuantumCraftSettings.CrystalPickaxeID + 256 ) {
+
+		if (FMLCommonHandler.instance().getEffectiveSide() == Side.CLIENT && mc.inGameHasFocus == true
+				&& mc.thePlayer.inventory.getCurrentItem() != null
+				&& mc.thePlayer.inventory.getCurrentItem().itemID == QuantumCraftSettings.CrystalPickaxeID + 256) {
 			final int damage = item.getItemDamageForDisplay();
-			final int damageLeft = 500-damage;
+			final int damageLeft = 500 - damage;
 			String damageString = null;
-			if (damageLeft >= 300){
+			if (damageLeft >= 300) {
 				damageString = "Â§a" + damageLeft + "/500";
-			}
-			else if (damageLeft >= 100){
+			} else if (damageLeft >= 100) {
 				damageString = "Â§e" + damageLeft + "/500";
-			}
-			else if (damageLeft >= 50){
+			} else if (damageLeft >= 50) {
 				damageString = "Â§5" + damageLeft + "/500";
 			}
 			return damageString;
-		}else{
+		} else {
 			return "";
 		}
-		
+
 	}
 
 	@Override
 	public void tickStart(EnumSet<TickType> type, Object... tickData) {
-	
 
 	}
 
 	@Override
 	public void tickEnd(EnumSet<TickType> type, Object... tickData) {
-		if((type.contains(TickType.CLIENT))&& (mc.theWorld != null)) {
-		if (mc.inGameHasFocus == true) {
+
+		if (mc.inGameHasFocus == true && FMLCommonHandler.instance().getEffectiveSide() == Side.CLIENT) {
 			ItemStack itemstack = mc.thePlayer.inventory.getCurrentItem();
 			mc.fontRenderer.drawString(getItemDamage(itemstack), 1, 1, 1);
-			//mc.fontRenderer.drawString("test", 1, 1, 1);
+			// mc.fontRenderer.drawString("test", 1, 1, 1);
 		}
-		}
+
 	}
 
 	@Override
