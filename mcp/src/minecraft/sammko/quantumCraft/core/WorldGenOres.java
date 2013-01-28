391d959582a20787644b7ddd21b8b7858b555df7
Theron Boerner <hunterboerner@gmail.com>
2013-01-28 15:53:02 -0600

    Fixed XYCraft spawning errors

--- mcp/src/minecraft/sammko/quantumCraft/core/WorldGenOres.java
+++ mcp/src/minecraft/sammko/quantumCraft/core/WorldGenOres.java
@@ -23,14 +23,15 @@ public class WorldGenOres implements IWorldGenerator {
 		
 		for(int i=0;i<1;i++){
 			ranomize(xo, zo, random);
-			if (o == 0) new WorldGenMinable(ItemInitializator.OrePositronium.blockID, 4 + random.nextInt(2)).generate(w, random, x, y, z); else
-			if (o == 1) new WorldGenMinable(ItemInitializator.OreRadium.blockID, 4 + random.nextInt(2)).generate(w, random, x, y, z); else
-			if (o == 2) new WorldGenMinable(ItemInitializator.OreGammatronium.blockID, 4 + random.nextInt(2)).generate(w, random, x, y, z); else
-			if (o == 3) new WorldGenMinable(ItemInitializator.OreNeutrinium.blockID, 4 + random.nextInt(2)).generate(w, random, x, y, z); else
-			if (o == 4) new WorldGenMinable(ItemInitializator.OreDepleted.blockID, 4 + random.nextInt(2)).generate(w, random, x, y, z); else
-			if (o == 5) new WorldGenMinable(ItemInitializator.OrePlutonium.blockID, 0 + random.nextInt(2)).generate(w, random, x, y, z);
+			if (o == 0) new WorldGenMinable(QuantumCraftSettings.OrePositroniumID, 4 + random.nextInt(2)).generate(w, random, x, y, z); else
+			if (o == 1) new WorldGenMinable(QuantumCraftSettings.OreRadiumID, 4 + random.nextInt(2)).generate(w, random, x, y, z); else
+			if (o == 2) new WorldGenMinable(QuantumCraftSettings.OreGammatroniumID, 4 + random.nextInt(2)).generate(w, random, x, y, z); else
+			if (o == 3) new WorldGenMinable(QuantumCraftSettings.OreNeutriniumID, 4 + random.nextInt(2)).generate(w, random, x, y, z); else
+			if (o == 4) new WorldGenMinable(QuantumCraftSettings.OreDepletedID, 4 + random.nextInt(2)).generate(w, random, x, y, z); else
+			if (o == 5) new WorldGenMinable(QuantumCraftSettings.OrePlutoniumID, 0 + random.nextInt(2)).generate(w, random, x, y, z);
 			
 			System.out.println("[QuantumCraft] Generated vein of "+o+". Its at " + x + ", " + z);
+			
 		}
 		
 	}
