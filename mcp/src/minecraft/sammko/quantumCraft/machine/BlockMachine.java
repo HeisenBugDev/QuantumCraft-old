8673b1e8ca0a91e4e0be76b7683ece940cd8b0d2
sammko <samko.cavoj@gmail.com>
2013-01-30 17:33:11 +0100

    Ehh

--- mcp/src/minecraft/sammko/quantumCraft/machine/BlockMachine.java
+++ mcp/src/minecraft/sammko/quantumCraft/machine/BlockMachine.java
@@ -70,46 +70,5 @@ public class BlockMachine extends BlockContainer {
 		return new TileEntityExtractor(var1,0);
 		//TODO: change this based on the metadata of this block.
 	}
-
-	@Override
-    public void breakBlock(World world, int x, int y, int z, int par5, int par6) {
-            dropItems(world, x, y, z);
-            super.breakBlock(world, x, y, z, par5, par6);
-    }
-	
-	private void dropItems(World world, int x, int y, int z){
-         Random rand = new Random();
-
-         TileEntity tileEntity = world.getBlockTileEntity(x, y, z);
-         if (!(tileEntity instanceof IInventory)) {
-                 return;
-         }
-         IInventory inventory = (IInventory) tileEntity;
-
-         for (int i = 0; i < inventory.getSizeInventory(); i++) {
-                 ItemStack item = inventory.getStackInSlot(i);
-
-                 if (item != null && item.stackSize > 0) {
-                         float rx = rand.nextFloat() * 0.8F + 0.1F;
-                         float ry = rand.nextFloat() * 0.8F + 0.1F;
-                         float rz = rand.nextFloat() * 0.8F + 0.1F;
-
-                         EntityItem entityItem = new EntityItem(world,
-                                         x + rx, y + ry, z + rz,
-                                         new ItemStack(item.itemID, item.stackSize, item.getItemDamage()));
-
-                         if (item.hasTagCompound()) {
-                                 entityItem.func_92014_d().setTagCompound((NBTTagCompound) item.getTagCompound().copy());
-                         }
-
-                         float factor = 0.05F;
-                         entityItem.motionX = rand.nextGaussian() * factor;
-                         entityItem.motionY = rand.nextGaussian() * factor + 0.2F;
-                         entityItem.motionZ = rand.nextGaussian() * factor;
-                         world.spawnEntityInWorld(entityItem);
-                         item.stackSize = 0;
-                }
-        }
- }
 	
 }
