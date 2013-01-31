f03826dcf5637c8e28328fbed39b72b531f9f658
Theron Boerner <hunterboerner@gmail.com>
2013-01-31 16:30:37 -0600

    Move dem stuffz

--- mcp/src/minecraft/sammko/quantumCraft/blocks/BlockQuantumBasicBlocks.java
+++ mcp/src/minecraft/sammko/quantumCraft/blocks/BlockQuantumBasicBlocks.java
@@ -24,8 +24,6 @@ public class BlockQuantumBasicBlocks extends Block {
 	
 	@Override
 	public int getBlockTextureFromSideAndMetadata (int side, int metadata) {
-		this.setResistance(10.0F);
-		this.setHardness(1.5F);
 		return BlockTextureMatrix.getIndex(BlockTextureMatrix.Deco[metadata]);
 		
 	}
