e186727c83303d7b6fc25b770e955c1167578ee7
sammko <samko.cavoj@gmail.com>
2013-02-23 09:36:33 +0100

    Allright, changed machine texture indexing.

--- mcp/src/minecraft/sammko/quantumCraft/render/RenderBlockMachine.java
+++ mcp/src/minecraft/sammko/quantumCraft/render/RenderBlockMachine.java
@@ -23,17 +23,15 @@ public class RenderBlockMachine extends RendererBase {
 
 	void setStuff(RenderBlocks renderer, IBlockAccess world, Block block, int x, int y, int z)
 	{
-		//this sets the tMap in block class.
-		//BUG: hmm the following code doesnt actually work. will need to debug it.
 		TileEntityMachine t =  (TileEntityMachine) world.getBlockTileEntity(x, y, z);
 		
 		tMap[0] = BlockTextureMatrix.getIndex(BlockTextureMatrix.EBlockBackSingle);
-		tMap[1] = BlockTextureMatrix.getIndex(BlockTextureMatrix.EBlockBackSingle);
+		tMap[1] = BlockTextureMatrix.getIndex(BlockTextureMatrix.MachineTop[0]);
 		
-		if (t.rotation == ForgeDirection.NORTH) tMap[2] = BlockTextureMatrix.getIndex(BlockTextureMatrix.QuantumGenerator); else tMap[2] = BlockTextureMatrix.getIndex(BlockTextureMatrix.EBlockSideSingle);
-		if (t.rotation == ForgeDirection.SOUTH) tMap[3] = BlockTextureMatrix.getIndex(BlockTextureMatrix.QuantumGenerator); else tMap[3] = BlockTextureMatrix.getIndex(BlockTextureMatrix.EBlockSideSingle);
-		if (t.rotation == ForgeDirection.WEST) tMap[4] = BlockTextureMatrix.getIndex(BlockTextureMatrix.QuantumGenerator); else tMap[4] = BlockTextureMatrix.getIndex(BlockTextureMatrix.EBlockSideSingle);
-		if (t.rotation == ForgeDirection.EAST) tMap[5] = BlockTextureMatrix.getIndex(BlockTextureMatrix.QuantumGenerator); else tMap[5] = BlockTextureMatrix.getIndex(BlockTextureMatrix.EBlockSideSingle);
+		if (t.rotation == ForgeDirection.NORTH) tMap[2] = BlockTextureMatrix.getIndex(BlockTextureMatrix.MachineFront[0]); else tMap[2] = BlockTextureMatrix.getIndex(BlockTextureMatrix.EBlockSideSingle);
+		if (t.rotation == ForgeDirection.SOUTH) tMap[3] = BlockTextureMatrix.getIndex(BlockTextureMatrix.MachineFront[0]); else tMap[3] = BlockTextureMatrix.getIndex(BlockTextureMatrix.EBlockSideSingle);
+		if (t.rotation == ForgeDirection.WEST) tMap[4] = BlockTextureMatrix.getIndex(BlockTextureMatrix.MachineFront[0]); else tMap[4] = BlockTextureMatrix.getIndex(BlockTextureMatrix.EBlockSideSingle);
+		if (t.rotation == ForgeDirection.EAST) tMap[5] = BlockTextureMatrix.getIndex(BlockTextureMatrix.MachineFront[0]); else tMap[5] = BlockTextureMatrix.getIndex(BlockTextureMatrix.EBlockSideSingle);
 		
 		t.tMap = this.tMap;
 	}
@@ -61,8 +59,8 @@ public class RenderBlockMachine extends RendererBase {
 	@Override
 	public void renderInventoryBlock(Block block, int metadata, int modelID, RenderBlocks renderer) {
 		super.renderInventoryBlock(block, metadata, modelID, renderer, new int[] { BlockTextureMatrix.getIndex(BlockTextureMatrix.EBlockBackSingle),
-				BlockTextureMatrix.getIndex(BlockTextureMatrix.EBlockBackSingle),BlockTextureMatrix.getIndex(BlockTextureMatrix.EBlockSideSingle),
-				BlockTextureMatrix.getIndex(BlockTextureMatrix.QuantumGenerator),BlockTextureMatrix.getIndex(BlockTextureMatrix.EBlockSideSingle),
+				BlockTextureMatrix.getIndex(BlockTextureMatrix.MachineTop[0]),BlockTextureMatrix.getIndex(BlockTextureMatrix.EBlockSideSingle),
+				BlockTextureMatrix.getIndex(BlockTextureMatrix.MachineFront[0]),BlockTextureMatrix.getIndex(BlockTextureMatrix.EBlockSideSingle),
 				BlockTextureMatrix.getIndex(BlockTextureMatrix.EBlockSideSingle) });
 	}
 }
