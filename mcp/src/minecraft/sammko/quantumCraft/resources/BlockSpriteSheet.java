06acb1f6a9dadda4450abcfa352c6ba58327d10c
Theron Boerner <hunterboerner@gmail.com>
2013-03-13 13:32:33 -0500

    grr

---
+++ mcp/src/minecraft/sammko/quantumCraft/resources/BlockSpriteSheet.java
@@ -0,0 +1,11 @@
+package sammko.quantumCraft.resources;
+
+import codechicken.core.render.SpriteSheetManager.SpriteSheet;
+
+public class BlockSpriteSheet extends SpriteSheet {
+
+	public BlockSpriteSheet() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ mcp/src/minecraft/sammko/quantumCraft/resources/SpriteSheets.java
@@ -0,0 +1,10 @@
+package sammko.quantumCraft.resources;
+
+import sammko.quantumCraft.core.QuantumCraftSettings;
+import codechicken.core.render.SpriteSheetManager.SpriteSheet;
+
+public class SpriteSheets {
+
+	public static SpriteSheet BlockSpriteSheet = new SpriteSheet(16,16,QuantumCraftSettings.BLOCK_PNG);
+
+}
